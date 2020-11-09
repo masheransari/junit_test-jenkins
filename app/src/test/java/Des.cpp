ff2d03fb0feb0de3b0c32d204c568ea9feb2a0c1
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2020-11-09 12:36:15 +0500

    Updated .gitignore file

---
+++ app/src/test/java/Des.cpp
@@ -0,0 +1,12 @@
+//
+// Created by asher on 11/8/2020.
+//
+
+#include "Des.h"
+
+#include <jni.h>
+#include <String>
+#include <iostream>
+
+extern "C" JNIEXPORT jstring JNICALL
+java_edu_ppu_someapp_MainActivity
---
+++ app/src/test/java/Des.h
@@ -0,0 +1,16 @@
+//
+// Created by asher on 11/8/2020.
+//
+
+#ifndef JUNIT_TEST_JENKINS_DES_H
+#define JUNIT_TEST_JENKINS_DES_H
+
+
+
+class Des {
+
+};
+
+
+
+#endif //JUNIT_TEST_JENKINS_DES_H
